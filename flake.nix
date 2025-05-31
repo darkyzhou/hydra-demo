774d49fcead8b4efb6e93fbfb36922b63c720881
darkyzhou <darkyoooooo@gmail.com>
2025-06-01 01:15:55 +0800

    update

--- flake.nix
+++ flake.nix
@@ -1,22 +1,18 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-
       packages.x86_64-linux.tree = nixpkgs.legacyPackages.x86_64-linux.tree;
-
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.tree;
-
-      hydraJobs."tester2" = self.defaultPackage;
-      hydraJobs."tester" = self.defaultPackage;
-      hydraJobs."tester-readme" = pkgs.runCommand "readme" { } ''
-        echo hello worl
-        mkdir -p $out/nix-support
-        echo "# A readme" > $out/readme.md
-        echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
-      '';
+      hydraJobs."test" = self.defaultPackage;
     };
 }
--- jobsets.nix
+++ jobsets.nix
@@ -1,35 +1,51 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 with pkgs;
 let
-  mkJobsets = drv {inherit lib writeText;};
-  drv = { lib, writeText }: { owner, repo, branches, pullRequests }:
+  mkJobsets = drv { inherit lib writeText; };
+  drv =
+    { lib, writeText }:
+    {
+      owner,
+      repo,
+      branches,
+      pullRequests,
+    }:
     let
-      toJobset = { url, ref }: {
-        checkinterval = 100;
-        emailoverride = "";
-        enabled = 1;
-        enableemail = false;
-        hidden = false;
-        type = 1;
-        keepnr = 10;
-        description = "branch-${ref}";
-        schedulingshares = 100;
-        flake = "github:tomberek/hydra-demo";
-      };
+      toJobset =
+        { url, ref }:
+        {
+          checkinterval = 100;
+          emailoverride = "";
+          enabled = 1;
+          enableemail = false;
+          hidden = false;
+          type = 1;
+          keepnr = 10;
+          description = "branch-${ref}";
+          schedulingshares = 100;
+          flake = "github:tomberek/hydra-demo";
+        };
 
-      branchToJobset = ref: toJobset {
-        url = "https://github.com/${owner}/${repo}.git";
-        inherit ref;
-      };
+      branchToJobset =
+        ref:
+        toJobset {
+          url = "https://github.com/${owner}/${repo}.git";
+          inherit ref;
+        };
 
-      pullRequestToJobset = n: pr: toJobset {
-        url = "https://github.com/${pr.base.repo.owner.login}/${pr.base.repo.name}.git";
-        ref = "pull/${n}/head";
-      };
+      pullRequestToJobset =
+        n: pr:
+        toJobset {
+          url = "https://github.com/${pr.base.repo.owner.login}/${pr.base.repo.name}.git";
+          ref = "pull/${n}/head";
+        };
 
       jobsetsAttrs =
-        lib.mapAttrs pullRequestToJobset (lib.importJSON pullRequests) // lib.genAttrs branches branchToJobset;
+        lib.mapAttrs pullRequestToJobset (lib.importJSON pullRequests)
+        // lib.genAttrs branches branchToJobset;
     in
     {
       jobsets = writeText "jobsets.json" (builtins.toJSON jobsetsAttrs);

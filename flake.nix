83789f4be3830b711d9d9b4c222767f15e60e7a9
darkyzhou <darkyoooooo@gmail.com>
2025-06-01 01:26:32 +0800

    update

--- flake.nix
+++ flake.nix
@@ -11,8 +11,8 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-      packages.x86_64-linux.tree = nixpkgs.legacyPackages.x86_64-linux.tree;
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.tree;
+      packages.x86_64-linux.top = nixpkgs.legacyPackages.x86_64-linux.top;
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.top;
       hydraJobs."test" = self.defaultPackage;
     };
 }
